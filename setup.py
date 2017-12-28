1d6d26e0f7f88707389df4e7f5c340cfb64906e1
k-nakayama <kotaro.nakayama@gmail.com>
2017-12-28 12:28:28 +0900

    changed distribution task to the latest approach.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='jupyter_pep8',
-    version='0.1.1',
+    version='0.1.2',
     description='Apply PEP8 for Jupyter Notebooks',
     url='http://github.com/napman/jupyter_pep8',
     author='Kotaro Nakayama',
--- tasks.py
+++ tasks.py
@@ -2,5 +2,4 @@ from invoke import task, MockContext, run
 
 @task
 def pypi(ctx):
-    run('python setup.py sdist')
-    run('twine upload dist/*')
+    run('python setup.py sdist upload')
