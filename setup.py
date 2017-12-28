e99a786ed930560576c5aa094c243ac91a3f59cd
k-nakayama <kotaro.nakayama@gmail.com>
2017-12-28 12:18:48 +0900

    added dependencies.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
 setup(name='jupyter_pep8',
-    version='0.1',
+    version='0.1.1',
     description='Apply PEP8 for Jupyter Notebooks',
     url='http://github.com/napman/jupyter_pep8',
     author='Kotaro Nakayama',
     author_email='kotaro.nakayama@gmail.com',
     license='BSD',
     scripts=['bin/jupyter_pep8'],
+    install_requires=[
+        "click",
+        "autopep8",
+    ],
     zip_safe=False)
